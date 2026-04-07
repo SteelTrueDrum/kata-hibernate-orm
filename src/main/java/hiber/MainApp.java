588f6f3886305a600412f02be211b4fece9c725b
SteelTrueDrum <steeltruedrum@gmail.com>
2026-04-07 21:20:38 +0300

    second commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,51 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+
+      userService.add(new User("User5", "Lastname5", "user5@mail.ru",
+              new Car("VW", 6)));
+      userService.add(new User("User6", "Lastname6", "user6@mail.ru",
+              new Car("Lada", 2121)));
+      userService.add(new User("User7", "Lastname7", "user7@mail.ru",
+              new Car("Skoda", 331)));
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+user.getCar());
+         System.out.println();
+      }
+
+      User user = userService.getUserByCar("VW", 6);
+      System.out.println(user.toString());
+      User user2 = userService.getUserByCar("Lada", 2121);
+      System.out.println(user2.toString());
+      User user3 = userService.getUserByCar("Skoda", 331);
+      System.out.println(user3.toString());
+
+      context.close();
+   }
+}
